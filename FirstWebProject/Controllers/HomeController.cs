f896921065a69f4d0bb2d868c1c4d67890cad3c7
MezemovRufetCode <rufatam@code.edu.az>
2021-12-16 13:55:40 +0400

    Add project files.

---
+++ FirstWebProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebProject.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "Index";
+            return view;
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
